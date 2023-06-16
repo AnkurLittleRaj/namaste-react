eff95182fdbd14e2b8b8fe4ae30e898af5d9fd77
AnkurLittleRaj <raj.ankur8@gmail.com>
2023-06-16 21:21:34 +0530

    changes after 3rd episode

--- App.js
+++ App.js
@@ -1,16 +1,24 @@
-const parent = React.createElement("div",{id:"parent"},
-[
-React.createElement("div",{id:"child"}, 
-[
-React.createElement("h1",{},"i am h1 tag"),React.createElement("h2",{},"i am h2 tag")]),
-React.createElement("div",{id:"child 2"}, 
-[
-React.createElement("h1",{},"i am h1 tag"),React.createElement("h2",{},"i am h2 tag")])]);
+import React from "react";
+import ReactDOM from "react-dom/client";
 // console.log("head",heading);
+const val =1000;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 
 
+const FunctionalHeading = () => (
+    <div id="container">
+      
+<h1> rendering Functional Component {val}</h1> 
+{test}
+    </div>
+);
+const test = (
+  <>
+      <h2>Test</h2>
+      <h2>hello</h2>
+  </>
+  
+);
 
-
-root.render(parent);
+root.render(<FunctionalHeading />);
